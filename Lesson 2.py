61c9f97eda1dece8c6e9a9a1823d240ccd26ee6f
KirHash <104854308+KirHash@users.noreply.github.com>
2024-03-29 03:02:34 +0530

    Add files via upload

---
+++ Lesson 2.py
@@ -0,0 +1,18 @@
+#Taking Input in Python
+
+# name = input()
+# age = int(input())
+# salary = float(input())
+# isAdmin = bool(input())
+
+# print(name, type(name))
+# print(age, type(age))
+# print(salary, type(salary))
+# print(isAdmin, type(isAdmin))
+
+# num = input().split(",")
+# print(num, type(num))
+
+lst = list(map(int,input().split()))
+print(lst)
+print(len(lst))
