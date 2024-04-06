7adbbec12fa2da45bf9a16fc93732e79717c8ce0
KirHash <104854308+KirHash@users.noreply.github.com>
2024-04-07 00:47:09 +0530

    Add files via upload

---
+++ Lesson 8.py
@@ -0,0 +1,27 @@
+# Basic Maths Problems
+
+# Count Digits
+# def countDigit(num):
+#     count = 0
+#     while num > 0:
+#         num = num//10
+#         count += 1
+#     return count
+
+# num = int(input())
+# ans = countDigit(num)
+# print(ans)
+
+# Reverse Number
+# def revNum(num):
+#     ans = 0
+#     while num > 0:
+#         rev = num%10
+#         ans = ans*10 + rev
+#         num = num//10
+#     return ans
+# num = int(input())
+# ans = revNum(num)
+# print(ans)
+
+
