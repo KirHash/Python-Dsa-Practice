03127eae649d939b1ca8bb1f506850006126f9bc
KirHash <104854308+KirHash@users.noreply.github.com>
2024-04-06 23:32:48 +0530

    Update Lesson 7.py

--- Lesson 7.py
+++ Lesson 7.py
@@ -178,6 +178,16 @@
 #     k -=1
 #     print()
 
+# n = 5
+# for i in range(n+1):
+#     if i % 2 == 0:
+#         for j in range(i, 0, -1):
+#             print(j, end="")
+#     else:
+#         for j in range(1, i+1):
+#             print(j, end="")
+#     print()
+
 
 
 
