4031674dd39b4f20cb43101dc8b75d82c23b01b1
KirHash <104854308+KirHash@users.noreply.github.com>
2024-04-15 01:23:16 +0530

    Update Lesson 8.py

--- Lesson 8.py
+++ Lesson 8.py
@@ -24,4 +24,39 @@
 # ans = revNum(num)
 # print(ans)
 
+#Pallindrome
+# def revNum(num):
+#     ans = 0
+#     while num > 0:
+#         rev = num%10
+#         ans = ans*10 + rev
+#         num = num//10
+#     return ans
+
+# def pallindrome(num):
+#     return num == revNum(num)
+
+# num = int(input())
+# ans = pallindrome(num)
+# print(ans)
+
+#GCD
+# def gcd(a,b):
+#     divisor = min(a,b)
+#     dividend = max(a,b)
+
+#     while dividend%divisor != 0:
+#         temp = dividend
+#         dividend = divisor
+#         divisor = temp%divisor
+#     return divisor
+
+# a = int(input())
+# b = int(input())
+
+# res = gcd(a,b)
+# print(res)
+
+
+
 
