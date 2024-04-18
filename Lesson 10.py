9c49ea79ed5a4f4e7e4f2332e1671aecf021ffd0
KirHash <104854308+KirHash@users.noreply.github.com>
2024-04-19 04:59:52 +0530

    Update Lesson 10.py

--- Lesson 10.py
+++ Lesson 10.py
@@ -45,3 +45,59 @@
 
 #NOTE: When operation executes after function, the execution of the code reverses (as the last operation executes firstly)
 
+#Sum of first n numbers
+# n = 10
+# for i in range(1, n+1):
+#     sm = 0
+#     if i <= n:
+#         sm = sm + i
+#         i += 1
+# print(sm)
+
+# def sumN(i, n, sm):
+#     if i > n:
+#         return sm
+#     return sumN(i+1, n, sm+i)
+
+# ans = sumN(1, 10, 0)
+# print(ans)
+
+# def sumN(i, n):
+#     if i > n:
+#         return 0
+#     res = sumN(i+1, n) + i
+#     return res
+
+# ans = sumN(1, 5)
+# print(ans)
+
+#Factorial of a number n(in forward...1*2*3*....*n)
+# n = 5
+# ans = 1
+# for i in range(1, n+1): 
+#     ans = ans * i
+# print(ans)
+
+
+# def factN(i, n):
+#     if i > n:
+#         return 1
+#     return i*factN(i+1, n)
+
+# ans = factN(1, 5)
+# print(ans)
+
+#Factorial of a number n(in reverse...n*n-1*n-2*......*2*1)
+# n = 5
+# ans = 1
+# for i in range(n, 0, -1):
+#     ans = ans * i
+# print(ans)
+
+# def factN(n):
+#     if n == 0:
+#         return 1
+#     return n*factN(n-1)
+
+# res = factN(5)
+# print(res)
